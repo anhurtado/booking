a416a69deecea465bf80051f3cb24b067348f927
Anthony Hurtado <shaka430@hotmail.com>
2021-07-13 00:59:15 -0400

    Created project on Angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AppRoutingModule } from './app.routing';
+import { AppComponent } from './app.component';
+import { AppService } from './app.service';
+import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+  ],
+  providers: [AppService],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
